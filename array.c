83705e9041248bb5dcc97a5f117cb20d7f7e31f8
Andrius Farias <72238314+FariaZss@users.noreply.github.com>
2025-07-30 16:06:35 -0300

    Update array.c

--- array.c
+++ array.c
@@ -8,9 +8,9 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_FUNCIONARIOS 5
+#define MAX_FUNCIONARIOS 2
 
-typedef struct Funcionarios{
+typedef struct{
     char nome[100], cargo[50];
     int salario, jornada;
     
@@ -18,7 +18,7 @@ typedef struct Funcionarios{
 
 int main()
 {
-    Funcionarios func[5];
+    FUNCIONARIOS func[5];
     
     for(int i=0;i<MAX_FUNCIONARIOS;i++){
         strcpy(func[i].nome, "NULL");
@@ -30,13 +30,19 @@ int main()
     for(int i=0;i<MAX_FUNCIONARIOS;i++){
         printf("Informe o nome do funcionario: "); scanf("%s", func[i].nome);
         printf("Informe o cargo do funcionario: "); scanf("%s", func[i].cargo);
-        printf("Informe o salario do funcionario: "); scanf("%d", func[i].salario);
-        printf("Informe a jornada em horas do funcionario: "); scanf("%d", func[i].jornada);
+        printf("Informe o salario do funcionario: "); scanf("%d", &func[i].salario);
+        printf("Informe a jornada em horas do funcionario: "); scanf("%d", &func[i].jornada);
+        
+        printf("Cadastro realizado\n\n");
     }
     
+    for(int i=0;i<MAX_FUNCIONARIOS;i++){
+        printf("Nome: %s\n", func[i].nome);
+        printf("Cargo: %s\n", func[i].cargo);
+        printf("Salario: %d\n", func[i].salario);
+        printf("Jornada: %d\n", func[i].jornada);
+    }
     
-    
-    
-    
+
     return 0;
 }
